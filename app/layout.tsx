97c7e34457d476fca2d8cd544bbe78a5ed4c176f
ryuk-D-wild <aiwarlog@gmail.com>
2025-10-01 23:35:35 +0530

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Blood detection',
 }
 
 export default function RootLayout({
@@ -19,7 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
